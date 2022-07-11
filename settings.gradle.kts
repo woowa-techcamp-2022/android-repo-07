833207a4859dc713ea4a7dd8851a8bc2201e1457
mdb1217 <70698151+mdb1217@users.noreply.github.com>
2022-07-11 17:26:32 +0900

    [ADD] #1 -그래들 라이브러리 추가 및 버전 추가

---
+++ settings.gradle.kts
@@ -0,0 +1,14 @@
+rootProject.name = "Github Application"
+
+enableFeaturePreview("VERSION_CATALOGS")
+
+fun includeProject(moduleName: String, rootFolderName: String = "") {
+    settings.include(moduleName)
+
+    if (rootFolderName.isNotEmpty()) {
+        project(moduleName).projectDir =
+            File(rootDir, "${rootFolderName}/${moduleName.substring(startIndex = 1)}")
+    }
+}
+
+includeProject(":app")
