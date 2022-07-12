032be416df34c9e3f45192ab3709b5d4bb21d972
안광민 <creativeduck0518@gmail.com>
2022-07-12 11:14:46 +0900

    [ADD] #11 API 연동을 위한 준비

--- app/src/main/java/org/woowatechcamp/githubapplication/GithubApplication.kt
+++ app/src/main/java/org/woowatechcamp/githubapplication/GithubApplication.kt
@@ -1,11 +1,35 @@
 package org.woowatechcamp.githubapplication
 
 import android.app.Application
+import android.content.SharedPreferences
 import dagger.hilt.android.HiltAndroidApp
+import androidx.security.crypto.EncryptedSharedPreferences
+import androidx.security.crypto.MasterKeys
 
 @HiltAndroidApp
 class GithubApplication : Application() {
     override fun onCreate() {
         super.onCreate()
+        application = this
+        val keyGenParameterSpec = MasterKeys.AES256_GCM_SPEC
+        val masterKeyAlias = MasterKeys.getOrCreate(keyGenParameterSpec)
+        Pref  = EncryptedSharedPreferences.create(
+        "important_data",
+        masterKeyAlias,
+        this,
+        EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
+        EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
+        )
+    }
+
+    companion object {
+        lateinit var application : GithubApplication
+        const val AUTH = "https://github.com/login/oauth/authorize"
+        const val ACCESS = "https://github.com/login/oauth/access_token"
+        const val BASE = "https://github.com/"
+        const val ACCESS_TOKEN = "access_token"
+        const val CODE = "code"
+        lateinit var Pref : SharedPreferences
     }
 }
+
--- build.gradle.kts
+++ build.gradle.kts
@@ -8,6 +8,7 @@ buildscript {
         classpath(libs.plugin.androidGradle)
         classpath(libs.plugin.kotlin)
         classpath(libs.plugin.hilt)
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10")
     }
 }
 
