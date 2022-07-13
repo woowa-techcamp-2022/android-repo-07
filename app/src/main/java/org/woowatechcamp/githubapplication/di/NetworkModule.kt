3a0c82f7d61d8c6bb1d36fa346366fe3b188d0fa
안광민 <creativeduck0518@gmail.com>
2022-07-13 10:29:53 +0900

    #13 GIthub Issue API 연동 위한 클래스 생성
    Service
    Repository
    ViewModel
    Interceptor

--- app/src/main/java/org/woowatechcamp/githubapplication/di/NetworkModule.kt
+++ app/src/main/java/org/woowatechcamp/githubapplication/di/NetworkModule.kt
@@ -6,35 +6,80 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
+import org.woowatechcamp.githubapplication.BuildConfig
 import org.woowatechcamp.githubapplication.GithubApplication
 import org.woowatechcamp.githubapplication.data.auth.AuthService
+import org.woowatechcamp.githubapplication.data.issue.IssueService
+import org.woowatechcamp.githubapplication.data.user.UserService
+import org.woowatechcamp.githubapplication.network.TokenInterceptor
+import org.woowatechcamp.githubapplication.util.AuthPreferences
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
+import javax.inject.Qualifier
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object NetworkModule {
 
+    @Qualifier
+    @Retention(AnnotationRetention.BINARY)
+    annotation class AuthRetrofit
+
+    @Qualifier
+    @Retention(AnnotationRetention.BINARY)
+    annotation class GithubRetrofit
+
+    @Qualifier
+    @Retention(AnnotationRetention.BINARY)
+    annotation class TokenInter
+
+    @TokenInter
     @Singleton
     @Provides
-    fun provideOkHttpClient(
+    fun providesTokenInterceptor(
+        preferences : AuthPreferences
+    ) : Interceptor = TokenInterceptor(preferences)
 
+    @Singleton
+    @Provides
+    fun providesOkHttpClient(
+        @TokenInter tokenInterceptor : Interceptor
     ) : OkHttpClient {
         return OkHttpClient.Builder()
             .readTimeout(5000, TimeUnit.MILLISECONDS)
             .connectTimeout(5000, TimeUnit.MILLISECONDS)
             .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
+            .addNetworkInterceptor(tokenInterceptor)
             .build()
     }
 
-    // 로그인 시에만 사용하는 거라, 이후 지울 수 있는 방식으로 변경하기
+    // 로그인 시에 사용하는 Retrofit
+    @AuthRetrofit
     @Singleton
     @Provides
-    fun provideRetrofit(
+    fun providesAuthRetrofit(
+       okHttpClient: OkHttpClient
+    ) : Retrofit {
+        val gson : Gson = GsonBuilder()
+            .setLenient()
+            .create()
+
+        return Retrofit.Builder()
+            .baseUrl(BuildConfig.GITHUB_BASE)
+            .client(okHttpClient)
+            .addConverterFactory(GsonConverterFactory.create(gson))
+            .build()
+    }
+    // API 연동을 위한 Retrofit
+    @GithubRetrofit
+    @Singleton
+    @Provides
+    fun providesGithubRetrofit(
         okHttpClient: OkHttpClient
     ) : Retrofit {
         val gson : Gson = GsonBuilder()
@@ -42,13 +87,28 @@ object NetworkModule {
             .create()
 
         return Retrofit.Builder()
-            .baseUrl(GithubApplication.BASE)
+            .baseUrl(BuildConfig.GITHUB_API)
             .client(okHttpClient)
             .addConverterFactory(GsonConverterFactory.create(gson))
             .build()
     }
+
     @Singleton
     @Provides
-    fun provideAuthService(retrofit: Retrofit) : AuthService =
+    fun providesAuthService(
+        @AuthRetrofit retrofit : Retrofit) : AuthService =
         retrofit.create(AuthService::class.java)
+
+    @Singleton
+    @Provides
+    fun providesIssueService(
+        @GithubRetrofit retrofit : Retrofit
+    ) : IssueService = retrofit.create(IssueService::class.java)
+
+    @Singleton
+    @Provides
+    fun providesUserService(
+        @GithubRetrofit retrofit : Retrofit
+    ) : UserService = retrofit.create(UserService::class.java)
+
 }
---
+++ app/src/main/java/org/woowatechcamp/githubapplication/presentation/MainViewModel.kt
@@ -0,0 +1,70 @@
+package org.woowatechcamp.githubapplication.presentation
+
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import org.woowatechcamp.githubapplication.data.issue.IssueRepository
+import org.woowatechcamp.githubapplication.data.issue.model.IssueResponseItem
+import org.woowatechcamp.githubapplication.data.user.UserRepository
+import org.woowatechcamp.githubapplication.data.user.model.UserResponse
+import retrofit2.http.Url
+import java.io.InputStream
+import java.net.URL
+import javax.inject.Inject
+
+@HiltViewModel
+class MainViewModel @Inject constructor(
+    private val issueRepository: IssueRepository,
+    private val userRepository: UserRepository
+) : ViewModel() {
+
+    private val _errorMessage = MutableLiveData<String>()
+    private val _issueList = MutableLiveData<List<IssueResponseItem>>()
+    private val _userProfile = MutableLiveData<Bitmap>()
+
+    val errorMessage : LiveData<String> = _errorMessage
+    val issueList : LiveData<List<IssueResponseItem>> = _issueList
+    val userProfile : LiveData<Bitmap> = _userProfile
+
+    // 사용자 정보 가져오기(프로필용)
+    fun getUser() = viewModelScope.launch {
+        try {
+            val response = userRepository.getUser()
+            val body = response.body()
+            if (response.isSuccessful && body != null) {
+                val url = body.avatar_url
+                withContext(Dispatchers.IO) {
+                    val mInputStream = URL(url).openStream()
+                    val mBitmap = BitmapFactory.decodeStream(mInputStream)
+                    _userProfile.postValue(mBitmap)
+                }
+            } else {
+                _errorMessage.postValue("사용자 정보를 가져오는 데 실패했습니다.")
+            }
+        } catch (e : Exception) {
+            _errorMessage.postValue(e.message)
+        }
+    }
+
+    // 이슈 가져오기
+    fun getIssues() = viewModelScope.launch {
+        try {
+            val response = issueRepository.getIssues()
+            val body = response.body()
+            if (response.isSuccessful && body != null) {
+                _issueList.postValue(body.toList())
+            } else {
+                _errorMessage.postValue("오류가 발생했습니다.")
+            }
+        } catch (e : Exception) {
+            _errorMessage.postValue(e.message)
+        }
+    }
+}
