b8355422464591f97ef18e6017d9732d5b9ba412
mdb1217 <70698151+mdb1217@users.noreply.github.com>
2022-07-20 16:52:30 +0900

    [feat] #29 - 프로필 뷰 화면 로직

--- app/src/main/java/org/woowatechcamp/githubapplication/presentation/profile/ProfileActivity.kt
+++ app/src/main/java/org/woowatechcamp/githubapplication/presentation/profile/ProfileActivity.kt
@@ -1,14 +1,19 @@
 package org.woowatechcamp.githubapplication.presentation.profile
 
+import android.content.Intent
+import android.content.Intent.EXTRA_EMAIL
+import android.net.Uri
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
-import coil.load
 import org.woowatechcamp.githubapplication.R
+import org.woowatechcamp.githubapplication.databinding.ActivityProfileBinding
 import org.woowatechcamp.githubapplication.presentation.user.model.UserModel
+import org.woowatechcamp.githubapplication.util.ext.startAction
+import org.woowatechcamp.githubapplication.util.ext.startMail
 
 class ProfileActivity : AppCompatActivity() {
-    private lateinit var binding: org.woowatechcamp.githubapplication.databinding.ActivityProfileBinding
+    private lateinit var binding: ActivityProfileBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,15 +21,28 @@ class ProfileActivity : AppCompatActivity() {
         binding.lifecycleOwner = this
         setContentView(binding.root)
 
-        binding.toolbarProfile.setNavigationOnClickListener {
-            finish()
-        }
+        initView()
+    }
 
-        val item = intent.getParcelableExtra<UserModel>(getString(R.string.profile_item))
-        item?.let {
-            binding.user = it
-            binding.tvPerson.text = "${it.followers} Followers ・ ${it.following} Following"
-            binding.ivProfile.load(it.imgUrl)
+    private fun initView() {
+        with(binding) {
+            toolbarProfile.setNavigationOnClickListener {
+                finish()
+            }
+
+            val item = intent.getParcelableExtra<UserModel>("profile_item")
+            item?.let { data ->
+                user = data
+                tvProfileLink.setOnClickListener {
+                    startAction(Pair(Intent.ACTION_VIEW, Uri.parse(data.blog)))
+                }
+
+                tvProfileEmail.setOnClickListener {
+                    startMail(
+                        Pair(EXTRA_EMAIL, data.email)
+                    )
+                }
+            }
         }
     }
 }
