5435f339b13ed4b4d85488a3bf0b877a6120c80d
안광민 <creativeduck0518@gmail.com>
2022-07-21 00:14:33 +0900

    [FEAT] Main test code 작성

---
+++ app/src/test/java/org/woowatechcamp/githubapplication/user/FakeMainRepository.kt
@@ -0,0 +1,33 @@
+package org.woowatechcamp.githubapplication.user
+
+import org.woowatechcamp.githubapplication.data.user.UserRepository
+import org.woowatechcamp.githubapplication.presentation.user.model.UserModel
+import org.woowatechcamp.githubapplication.util.UiState
+import org.woowatechcamp.githubapplication.util.getOrError
+
+class FakeMainRepository : UserRepository {
+
+    private var returnError = false
+
+    private var userModel : UserModel? = null
+
+    override suspend fun getUser(): UiState<UserModel> {
+        if (returnError) {
+            return UiState.Error("Test Error")
+        }
+        return try {
+            UiState.Success(
+                userModel.getOrError("Test Null"))
+        } catch (e : Exception) {
+            UiState.Error(e.message ?: "Test Exception")
+        }
+    }
+
+    fun addUser(value : UserModel) {
+        userModel = value
+    }
+
+    fun setReturnError(value : Boolean) {
+        returnError = value
+    }
+}
